e8907328e7ddee126d423f0ce9a7eb4e7577f27e
sreenath <madhyastha@yahoo.com>
2025-11-07 17:09:44 -0500

    (feat): New Addition

--- config-list/obj/Debug/net9.0-windows/config-list.AssemblyInfo.cs
+++ config-list/obj/Debug/net9.0-windows/config-list.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("config-list")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0c928beaaca539e33a554d764d566921d46418a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d28769dd0943feb70f115f81a62eebabc3fe9eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("config-list")]
 [assembly: System.Reflection.AssemblyTitleAttribute("config-list")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
