fce92aef196287006b239241eb43989989d41107
sreenath <madhyastha@yahoo.com>
2025-11-16 19:14:32 -0500

    remove

--- config-list/obj/Debug/net9.0-windows/config-list.AssemblyInfo.cs
+++ config-list/obj/Debug/net9.0-windows/config-list.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("config-list")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b6534ec3255b499e4422c514b98587762af8a87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6e124e0ca572d86f5e13ba2978974f2d9b6e756")]
 [assembly: System.Reflection.AssemblyProductAttribute("config-list")]
 [assembly: System.Reflection.AssemblyTitleAttribute("config-list")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
